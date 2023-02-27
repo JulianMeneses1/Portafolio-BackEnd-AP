f81febe8e77d50c08860589c1afa535928d5c09a
JulianMeneses1 <julian.meneses11@gmail.com>
2023-02-27 16:41:02 -0300

    Actualizacion base de datos y MER, y subida de diagrama de clases

--- Base de Datos Portfolio.sql
+++ Base de Datos Portfolio.sql
@@ -3,7 +3,8 @@ use ap_portfolio;
 
 create table Usuarios (
 usuario varchar(50) PRIMARY KEY NOT NULL,
-contraseña varchar(50)
+contraseña varchar(50),
+estado_login boolean
 );
 
 create table Acerca_de_mi (
@@ -14,29 +15,9 @@ descripcion varchar(200)
 
 create table Conocimientos (
 id INT PRIMARY KEY NOT NULL auto_increment,
-titulo varchar(50),
-icono varchar(100)
-);
-
-create table Experiencia_laboral (
-id INT PRIMARY KEY NOT NULL auto_increment,
-nombreEmpresa varchar(50),
-descripcion varchar(200),
-fechaInicio DATE,
-fechaFin DATE
-);
-
-create table Formacion_academica (
-id INT PRIMARY KEY NOT NULL auto_increment,
-titulo varchar(50),
-descripcion varchar(200)
-);
-
-create table Proyectos (
-id INT PRIMARY KEY NOT NULL auto_increment,
-titulo varchar(50),
-descripcion varchar(200),
-imagen varchar(100)
+nombre varchar(50),
+icono varchar(100),
+titulo varchar(30)
 );
 
 create table Banner (
@@ -55,13 +36,9 @@ email varchar(50) UNIQUE,
 banner_id INT,
 usuario varchar(50),
 acerca_de_mi_id INT,
-formacion_academica_id INT,
-experiencia_laboral_id INT,
 foreign key (banner_id) references Banner (id),
 foreign key (usuario) references Usuarios (usuario),
-foreign key (acerca_de_mi_id) references Acerca_de_mi (id),
-foreign key (formacion_academica_id) references Formacion_academica (id),
-foreign key (experiencia_laboral_id) references Experiencia_laboral (id)
+foreign key (acerca_de_mi_id) references Acerca_de_mi (id)
 );
 
 create table Conocimientos_Personas (
@@ -70,4 +47,36 @@ personas_id INT,
 conocimientos_id INT,
 foreign key (personas_id) references Personas (id),
 foreign key (conocimientos_id) references Conocimientos(id)
+);
+
+create table Experiencia_laboral (
+id INT PRIMARY KEY NOT NULL auto_increment,
+titulo varchar(30),
+nombre_empresa varchar(50),
+descripcion varchar(200),
+fecha_inicio DATE,
+fecha_fin DATE,
+puesto varchar(100),
+url varchar(100),
+personas_id int,
+foreign key (personas_id) references Experiencia_laboral (id)
+);
+
+create table Formacion_academica (
+id INT PRIMARY KEY NOT NULL auto_increment,
+titulo varchar(30),
+nombre varchar(50),
+descripcion varchar(200),
+personas_id int,
+foreign key (personas_id) references Formacion_academica (id)
+);
+
+create table Proyectos (
+id INT PRIMARY KEY NOT NULL auto_increment,
+titulo varchar(30),
+nombre varchar(50),
+descripcion varchar(200),
+imagen varchar(100),
+personas_id int,
+foreign key (personas_id) references Proyectos (id)
 )
