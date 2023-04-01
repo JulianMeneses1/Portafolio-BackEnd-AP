c5e5bba9a1f0143cbb8db91de14ad65f656acba2
JulianMeneses1 <julian.meneses11@gmail.com>
2023-04-01 11:15:21 -0300

    creacion proyecto en spring boot

---
+++ Spring Boot/PortfolioWeb/src/test/java/com/backend/PortfolioWeb/PortfolioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.PortfolioWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
