bd87dea251f6e422abfc19b78c6d664761bd6709
JulianMeneses1 <julian.meneses11@gmail.com>
2023-05-30 17:53:43 -0300

    dando acceso a la documentacion en swagger

--- Portafolio/src/main/java/com/julianmeneses/proyectoSpringBoot/ProyectoSpringBootApplication.java
+++ Portafolio/src/main/java/com/julianmeneses/proyectoSpringBoot/ProyectoSpringBootApplication.java
@@ -1,9 +1,18 @@
 package com.julianmeneses.proyectoSpringBoot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition (
+        info = @Info(
+            title="App Portafolio",
+            version = "1.0.0",
+            description = "Mi CV online"             
+        )
+)
 public class ProyectoSpringBootApplication {
 
 	public static void main(String[] args) {
