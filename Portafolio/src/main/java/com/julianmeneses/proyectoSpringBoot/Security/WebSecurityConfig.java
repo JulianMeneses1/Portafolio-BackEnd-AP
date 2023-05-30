44050cbe468bce92ac54e70da0d1098eb8c449bc
JulianMeneses1 <julian.meneses11@gmail.com>
2023-05-30 18:50:01 -0300

    reconfigurando rutas permitidas

--- Portafolio/src/main/java/com/julianmeneses/proyectoSpringBoot/Security/WebSecurityConfig.java
+++ Portafolio/src/main/java/com/julianmeneses/proyectoSpringBoot/Security/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig {
         http.cors().and().csrf().disable().authorizeHttpRequests((authorize) -> {
             try {
                 authorize
-                        .requestMatchers("/obtener/**", "/enviarmail","/swagger-ui/**", "/v3/api-docs/swagger-config")
+                        .requestMatchers("/obtener/**", "/enviarmail","/swagger-ui/**", "/v3/**")
                         .permitAll()  // todas las request que empiecen con obtener (todos los GET) no requieren token
                         .requestMatchers("/editar/**", "/crear/**", "/eliminar/**", "/subir/archivo")
                         .authenticated() // todas las request que empiecen con editar, eliminar o crear están protegidas, requieren un token (todos los PUT, DELETE y POST)
